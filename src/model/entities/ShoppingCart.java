d1d568b78e6ca280ff0ec9d7913c8fc0653c35a1
Thiago Nakamura <thiagokenzo@outlook.com>
2025-04-07 17:03:02 -0300

    feat: create ShoppingCart class with product list, total calculation and display

---
+++ src/model/entities/ShoppingCart.java
@@ -0,0 +1,43 @@
+package model.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//This class represents a shopping cart that holds a list of products.
+public class ShoppingCart {
+
+	// List to store products added to the cart. It uses the Product interface.
+	private List<Product> products = new ArrayList<Product>();
+
+	public List<Product> getProducts() {
+		return products;
+	}
+	
+	// Adds a product to the shopping cart.
+	public void addProduct(Product product) {
+		products.add(product);
+	}
+	
+	// Removes a product to the shopping cart.
+	public void removeProduct(Product product) {
+		products.remove(product);
+	}
+	
+	// Calculates and returns the total price of all products in the cart.
+	public double getTotal() {
+		double sum = 0;
+		for (Product x : products) {
+			sum += x.getPrice();
+		}
+		return sum;
+	}
+	
+	// Displays all products currently in the cart.
+	public void displayProducts() {
+		System.out.println("------------------------");
+		System.out.println("Products in cart:");
+		for(Product x : products) {
+			System.out.println(x);
+		}
+	}
+}
